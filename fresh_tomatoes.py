513ccc1bc5f4bd9ec9c4b85b0de371ffbb867b47
ne9een <neginasghari@gmail.com>
2015-11-23 00:53:12 +0100

    Update fresh_tomatoes.py

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -11,10 +11,14 @@ main_page_head = """
     <meta charset="utf-8">
     <title>Favorite Movie!</title>
     <!-- Bootstrap 3 -->
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
+    <link rel="stylesheet"
+    href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+    <link rel="stylesheet"
+    href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
     <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+    <script
+    src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js">
+    </script>
     <style type="text/css" media="screen">
         body {
             padding-top: 80px;
@@ -58,16 +62,20 @@ main_page_head = """
     </style>
     <script type="text/javascript" charset="utf-8">
         // Pause the video when the modal is closed
-        $(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
-            // Remove the src so the player itself gets removed, as this is the only
+        $(document).on('click', '.hanging-close, .modal-backdrop,
+        .modal', function (event) {
+            // Remove the src so the player itself gets removed,
+            //as this is the only
             // reliable way to ensure the video stops playing in IE
             $("#trailer-video-container").empty();
         });
         // Start playing the video whenever the trailer modal is opened
         $(document).on('click', '.movie-tile', function (event) {
             var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
-            var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
-            $("#trailer-video-container").empty().append($("<iframe></iframe>", {
+            var sourceUrl = 'http://www.youtube.com/embed/' +
+            trailerYouTubeId + '?autoplay=1&html5=1';
+            $("#trailer-video-container").empty().append(
+            $("<iframe></iframe>", {
               'id': 'trailer-video',
               'type': 'text-html',
               'src': sourceUrl,
@@ -92,8 +100,9 @@ main_page_content = '''
     <div class="modal" id="trailer">
       <div class="modal-dialog">
         <div class="modal-content">
-          <a href="#" class="hanging-close" data-dismiss="modal" aria-hidden="true">
-                <img src="https://lh5.ggpht.com/v4-628SilF0HtHuHdu5EzxD7WRqOrrTIDi_MhEG6_qkNtUK5Wg7KPkofp_VJoF7RS2LhxwEFCO1ICHZlc-o_=s0#w=24&h=24"/>
+          <a href="#" class="hanging-close"
+          data-dismiss="modal" aria-hidden="true">
+                <img src="https://goo.gl/KnEXhq"/>
               </a>
           <div class="scale-media" id="trailer-video-container">
           </div>
@@ -105,12 +114,15 @@ main_page_content = '''
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
             <div class="navbar-header col-md-6">
-                <img  class="img-responsive " src="https://thegothamrogue.files.wordpress.com/2014/01/movies_logo.gif" alt="Movie logo" width="120" height="80" vspace="10">
+                <img  class="img-responsive "
+                src="https://thegothamrogue.files.wordpress.com/2014/01/movies_logo.gif"
+                alt="Movie logo" width="120" height="80" vspace="10">
             </div>
             <div class="col-md-6 text-right text-uppercase">
-                <h2 class="title-super text-thin"><font color="white">Negin Asghari </font></h2>
+                <h2 class="title-super text-thin"><font color="white">
+                Negin Asghari </font></h2>
                 <h4><font color="white">Favorite Movies</font></h4>
-            </div> 
+            </div>
         </div>
       </div>
     </div>
@@ -123,12 +135,16 @@ main_page_content = '''
 
 # A single movie entry html template
 movie_tile_content = '''
-    <div class="col-md-3 movie-tile text-left" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer ">
+    <div class="col-md-3 movie-tile text-left"
+    data-trailer-youtube-id="{trailer_youtube_id}"
+    data-toggle="modal" data-target="#trailer ">
         <img src="{poster_image_url}" width="220" height="342">
         <h4> <strong>{movie_title} </strong></h4>
-        <div> <h4 class="text-left" data-toggle="modal" data-target="trailer">{movie_storyline}</h4></div>
+        <div> <h4 class="text-left" data-toggle="modal"
+        data-target="trailer">{movie_storyline}</h4></div>
     </div>
 '''
+
 
 def create_movie_tiles_content(movies):
     # The HTML content for this section of the page
@@ -145,7 +161,7 @@ def create_movie_tiles_content(movies):
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
             movie_title=movie.title,
-			movie_storyline=movie.storyline,
+            movie_storyline=movie.storyline,
             poster_image_url=movie.poster_image_url,
             trailer_youtube_id=trailer_youtube_id
         )
